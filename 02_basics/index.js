48595073745651d21774305852385b5c5e77e833
prajwal-code-byte <prajwal.more@intellipaat.com>
2026-02-17 04:20:50 +0000

    change

--- 02_basics/index.js
+++ 02_basics/index.js
@@ -1,12 +1,16 @@
-const accountId = 1234567890
-let accountName = 'Intellipaat'
-var accoutEmail = 'google@gmail.com'
-accountCity = 'Bengaluru'
+// const accountId = 1234567890
+// let accountName = ''
+// var accoutEmail = 'google@gmail.com'
+// accountCity = 'Bengaluru'
 
-accountName = 'helloworld'
-// accountId = 0987654321
-accountName = 'S-Vyasa'
+// accountName = 'helloworld'
+// // accountId = 0987654321
+// accountName = 'S-Vyasa'
 
-// console.log(accountId);
-console.log(accountName);
+// // console.log(accountId);
+// console.log(accountName);
 
+
+let accoutName = ''
+
+console.log(accoutName);
