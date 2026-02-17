df5338ec754bf44d1b867a3abc1bec97ab54224e
prajwal-code-byte <prajwal.more@intellipaat.com>
2026-02-17 04:02:05 +0000

    const let

---
+++ 02_basics/index.js
@@ -0,0 +1,12 @@
+const accountId = 1234567890
+let accountName = 'Intellipaat'
+var accoutEmail = 'google@gmail.com'
+accountCity = 'Bengaluru'
+
+accountName = 'helloworld'
+// accountId = 0987654321
+accountName = 'S-Vyasa'
+
+// console.log(accountId);
+console.log(accountName);
+
